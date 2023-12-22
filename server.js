661e7615397cbc6d165c4c4ef2715182b413c9ae
Md Anas Sabah <mdanassabah@gmail.com>
2023-12-22 23:11:25 +0530

    created server for json-serber

---
+++ server.js
@@ -0,0 +1,23 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./data/questions.json");
+const cors = require("cors");
+
+server.use(cors());
+
+const middleware = jsonServer.defaults({
+  static: "./build",
+});
+
+const port = process.env.PORT || 9000;
+server.use(middleware);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
+server.use(router);
+server.listen(port, () => {
+  console.log(`Server is active at ${port}`);
+});
